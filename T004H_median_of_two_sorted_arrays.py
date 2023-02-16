928d77a28bebf24b55d39c4cfa5f414d9e858b83
vhqr0 <zq_cmd@163.com>
2023-02-16 19:28:40 +0800

    improve 4 median of two arrays, 15 3sum

--- T004H_median_of_two_sorted_arrays.py
+++ T004H_median_of_two_sorted_arrays.py
@@ -7,30 +7,27 @@ class Solution:
                                nums2: List[int]) -> float:
         l1, l2 = len(nums1), len(nums2)
 
-        if l1 == 0 and l2 == 0:
+        if l1 > l2:
+            l1, l2 = l2, l1
+            nums1, nums2 = nums2, nums1
+
+        if l2 == 0:
             return 0.0
+
         if l1 == 0:
             if l2 & 1 == 1:
                 return nums2[l2 // 2]  # odd
             return (nums2[l2 // 2 - 1] + nums2[l2 // 2]) / 2.0
-        if l2 == 0:
-            if l1 & 1 == 1:
-                return nums1[l1 // 2]  # odd
-            return (nums1[l1 // 2 - 1] + nums1[l1 // 2]) / 2.0
 
-        if l1 > l2:  # ensure l1 <= l2
-            l1, l2 = l2, l1
-            nums1, nums2 = nums2, nums1
-
-        t = (l1 + l2) // 2
+        half = (l1 + l2) // 2
         beg, end = 0, l1
         while beg < end:
             # nums1[:i] | nums1[i:]
             # nums2[:j] | nums2[j:]
-            #   len(nums1[:i]) + len(nums2[:j]) == t
-            #   len(nums1[i:]) + len(nums2[j:]) == t if even else t+1
+            #   len(nums1[:i]) + len(nums2[:j]) == half
+            #   len(nums1[i:]) + len(nums2[j:]) == half if even else half+1
             i = (beg + end) // 2
-            j = t - i
+            j = half - i
             if nums1[i] < nums2[j - 1]:
                 beg = i + 1
             else:
@@ -39,28 +36,23 @@ class Solution:
         if (l1 + l2) & 1 == 1:  # odd
             # min of right part
             if end == l1:
-                return nums2[t - end]
-            return min(nums1[end], nums2[t - end])
-
-        # nums2[:j] | nums2[j:]
-        #   make sure both parts are nonempty in nums2
-        #   case l1 == l2 and nums1 < nums2 or nums1 > nums2
-        if l1 == l2:
-            if end == 0:
-                return (nums1[0] + nums2[l2 - 1]) / 2.0
-            if end == l1:
-                return (nums1[l1 - 1] + nums2[0]) / 2.0
+                return nums2[half - end]
+            return min(nums1[end], nums2[half - end])
 
         # max of left part
         if end == 0:
-            r1 = nums2[t - end - 1]
+            r1 = nums2[half - 1]
+        elif end == l1 and l1 == l2:
+            r1 = nums1[l1 - 1]
         else:
-            r1 = max(nums1[end - 1], nums2[t - end - 1])
+            r1 = max(nums1[end - 1], nums2[half - end - 1])
 
         # min of right part
         if end == l1:
-            r2 = nums2[t - end]
+            r2 = nums2[half - l1]
+        elif end == 0 and l1 == l2:
+            r2 = nums1[0]
         else:
-            r2 = min(nums1[end], nums2[t - end])
+            r2 = min(nums1[end], nums2[half - end])
 
         return (r1 + r2) / 2.0
--- T015M_3sum.py
+++ T015M_3sum.py
@@ -1,71 +1,32 @@
-from typing import List, Set
-
-
-def uniq(nums: List[int]) -> List[int]:
-    # same numbers at most 2
-    nums_uniq = []
-    i = 0
-    while i < len(nums):
-        num = nums[i]
-        nums_uniq.append(num)
-        if i + 1 < len(nums) and nums[i + 1] == num:
-            nums_uniq.append(num)
-            while i < len(nums) and nums[i] == num:
-                i += 1
-        else:
-            i += 1
-    return nums_uniq
-
-
-def twoSum(nums: List[int], targets: Set[int], res: List[List[int]]):
-    i = 0
-    while i < len(nums):
-        num1 = nums[i]
-        j = i + 1
-        while j < len(nums):
-            num2 = nums[j]
-            num = num1 + num2
-            if -num in targets:
-                res.append([num1, num2, -num])
-            # skip dup numbers
-            if j + 1 < len(nums) and nums[j + 1] == num2:
-                j += 2
-            else:
-                j += 1
-        # skip dup numbers
-        if i + 1 < len(nums) and nums[i + 1] == num1:
-            i += 2
-        else:
-            i += 1
+from typing import List
 
 
 class Solution:
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
         res = []
 
-        zeros = nums.count(0)
-        nums1 = sorted(num for num in nums if num < 0)
-        nums2 = sorted(num for num in nums if num > 0)
+        for i in range(len(nums)):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
 
-        if len(nums1) > len(nums2):  # ensure len(nums1) <= len(nums2)
-            nums1, nums2 = nums2, nums1
+            j, k = i + 1, len(nums) - 1
 
-        nums1_uniq = uniq(nums1)
-        nums2_uniq = uniq(nums2)
-
-        nums1_set = set(nums1_uniq)
-        nums2_set = set(nums2_uniq)
-
-        if zeros >= 3:
-            res.append([0, 0, 0])
-
-        if zeros >= 1:
-            for num in nums1_set:
-                if -num in nums2_set:
-                    res.append([-num, 0, num])
-
-        twoSum(nums1_uniq, nums2_set, res)
-        twoSum(nums2_uniq, nums1_set, res)
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s > 0:
+                    k -= 1
+                elif s < 0:
+                    j += 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    while nums[j - 1] == nums[j] and j < k:
+                        j += 1
+                    k -= 1
+                    while nums[k] == nums[k + 1] and j < k:
+                        k -= 1
 
         return res
